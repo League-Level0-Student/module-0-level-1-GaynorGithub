ce0168709f2ff6fa9e27053b125d9d245b936ed3
codeavenger1 <league@WTS10.attlocal.net>
2017-10-30 18:26:50 -0700

    Badgers, Checkpoint 1

--- src/Badgers.java
+++ src/Badgers.java
@@ -1,8 +1,45 @@
 
 public class Badgers {
 public static void main(String[] args) {
-	
-	
-	
+	bgr(10);
+	mush(2);
+	bgr(10);
+	mush(2);
+	bgr(10);
+	mush(2);
+	bgr(10);
+	snak(2);
+	snak(3);
+	bgr(10);
+	mush(2);
 }
+
+static void bgr(int i) {
+	for (; i > 0; i--) {
+		System.out.print("badger, ");
+	}
+	System.out.println();
 }
+static void mush(int j) {
+	for (; j > 0; j--) {
+		System.out.print("mushroom, ");
+	}
+	System.out.println();
+}
+static void snak(int k) {
+	for (; k > 0; k--) {
+		System.out.print("snake, ");
+	}
+	System.out.println();
+}
+
+
+
+
+}
+
+
+
+
+
+
